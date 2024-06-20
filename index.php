89a5f23c2baa74815ad8aa810422ebbb914849e1
MicheleRaisa95 <micheleraisa95@gmail.com>
2024-06-20 17:28:21 +0200

    upd aggiunte cdn di bootstrap,axios e vue

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Dischi JSON</title>
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- /bootstrap -->
+</head>
+<body>
+    <div id="app">
+        <header>
+            <div class="container p-5 text-center">
+                <h1>{{title}}</h1>
+            </div>
+        </header>
+    </div>
+
+    <!-- vue -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <!-- /vue -->
+
+    <!-- axios -->
+     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+     <!-- /axios -->
+
+    <!-- js -->
+    <script src="./js/main.js"></script>
+    <!-- /js -->
+
+</body>
+</html>
